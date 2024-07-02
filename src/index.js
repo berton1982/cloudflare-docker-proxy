88a699630adec73c75a3658d6482a0cc39e6cb3c
berton1982 <berton.jiang@outlook.com>
2024-07-02 22:07:34 +0800

    Update index.js
    修改域名为bpood.com

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bpood.com": "https://registry-1.docker.io",
+  "quay.bpood.com": "https://quay.io",
+  "gcr.bpood.com": "https://gcr.io",
+  "k8s-gcr.bpood.com": "https://k8s.gcr.io",
+  "k8s.bpood.com": "https://registry.k8s.io",
+  "ghcr.bpood.com": "https://ghcr.io",
+  "cloudsmith.bpood.com": "https://docker.cloudsmith.io",
+  "ecr.bpood.com": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
